6153057b2f8cd55697891ab98a161128349db6cb
Stéphane Deverly <steph@stephmac.local>
2012-12-11 10:28:09 +0100

    decouple attribute and entity change callbacks revival / retirement seems to work still need to check script_path change CB

--- src/plugins/pluginManager.py
+++ src/plugins/pluginManager.py
@@ -45,12 +45,18 @@ def registerCallbacks(reg):
         reg.logger.debug( "Using %s %s" % ( k, settings[k] ) )
     settings['engine'] = reg.getEngine()
     # Register all callbacks related to our custom entity
-    events = [ r'Shotgun_%s_New', r'Shotgun_%s_Change', r'Shotgun_%s_Retirement', r'Shotgun_%s_Revival']
-    eventFilter = {}
-    for e in events :
-        eventFilter[ e % settings['sgEntity'] ] = ['sg_status_list', 'retirement_date', 'sg_script_path' ] # retirement_date
-    reg.logger.debug( "Registering %s" % eventFilter)
-    reg.registerCallback( settings['script_name'], settings['script_key'], pluginEventCB, eventFilter, settings )
+    # Attribute change callback
+    eventFilter = { r'Shotgun_%s_Change' % settings['sgEntity'] : ['sg_status_list', 'sg_script_path' ] }
+    reg.logger.debug("Registring %s", eventFilter )
+    reg.registerCallback( settings['script_name'], settings['script_key'], changeEventCB, eventFilter, settings )
+    # Entity change callbacks
+    eventFilter = {
+        r'Shotgun_%s_New' % settings['sgEntity'] : None,
+        r'Shotgun_%s_Retirement' % settings['sgEntity'] : None,
+        r'Shotgun_%s_Revival' % settings['sgEntity'] : None
+        }
+    reg.logger.debug("Registring %s", eventFilter )
+    reg.registerCallback( settings['script_name'], settings['script_key'], entityEventCB, eventFilter, settings )
 
     # Get a list of all the existing plugins from Shotgun
     sgHandle = sg.Shotgun( reg.getEngine().config.getShotgunURL(), settings['script_name'], settings['script_key'] )
@@ -65,7 +71,7 @@ def registerCallbacks(reg):
     # for enabling and disabling debugging on a per plugin basis.
     #reg.logger.setLevel(logging.ERROR)
 
-def pluginEventCB(sg, logger, event, args):
+def changeEventCB(sg, logger, event, args):
     """
     A callback that logs its arguments.
 
@@ -78,31 +84,53 @@ def pluginEventCB(sg, logger, event, args):
     etype = event['event_type']
     attribute = event['attribute_name']
     entity = event['entity']
-    if re.search( 'Change$', etype ) :
-        if attribute == 'sg_status_list' :
-            logger.info( "Status changed for %s", entity['name'] )
-            # We need some details to know what to do
-            p = sg.find_one( entity['type'], [[ 'id', 'is', entity['id']]], ['sg_script_path'] )
-            if p['sg_script_path'] and p['sg_script_path']['local_path'] :
-                if event['meta']['new_value'] == 'act' :
-                    logger.info('Loading %s', p['sg_script_path']['name'])
-                    args['engine'].loadPlugin( p['sg_script_path']['local_path'], autoDiscover=False)
-                else : #Disable the plugin
-                    logger.info('Unloading %s', p['sg_script_path']['name'])
-                    args['engine'].unloadPlugin( p['sg_script_path']['local_path'])
-        elif attribute == 'sg_script_path' : # Should unload and reload the plugin
-            pass
-    elif e.search( 'Retirement$', etype ) : # Unload the plugin
+    if attribute == 'sg_status_list' :
+        logger.info( "Status changed for %s", entity['name'] )
+        # We need some details to know what to do
         p = sg.find_one( entity['type'], [[ 'id', 'is', entity['id']]], ['sg_script_path'] )
+        if p['sg_script_path'] and p['sg_script_path']['local_path'] :
+            if event['meta']['new_value'] == 'act' :
+                logger.info('Loading %s', p['sg_script_path']['name'])
+                args['engine'].loadPlugin( p['sg_script_path']['local_path'], autoDiscover=False)
+            else : #Disable the plugin
+                logger.info('Unloading %s', p['sg_script_path']['name'])
+                args['engine'].unloadPlugin( p['sg_script_path']['local_path'])
+    elif attribute == 'sg_script_path' : # Should unload and reload the plugin
+        logger.info( "Script path changed for %s", entity['name'] )
+        # We need some details to know what to do
+        p = sg.find_one( entity['type'], [[ 'id', 'is', entity['id']]], ['sg_status_list'] )
+        spath = event['meta']['new_value']
+        if p['sg_script_path'] and p['sg_script_path']['local_path'] :
+            if event['meta']['new_value'] == 'act' :
+                logger.info('Loading %s', p['sg_script_path']['name'])
+                args['engine'].loadPlugin( p['sg_script_path']['local_path'], autoDiscover=False)
+            else : #Disable the plugin
+                logger.info('Unloading %s', p['sg_script_path']['name'])
+                args['engine'].unloadPlugin( p['sg_script_path']['local_path'])
+
+         
+def entityEventCB(sg, logger, event, args):
+    """
+    A callback that logs its arguments.
+
+    @param sg: Shotgun instance.
+    @param logger: A preconfigured Python logging.Logger object
+    @param event: A Shotgun event.
+    @param args: The args passed in at the registerCallback call.
+    """
+    logger.debug("%s" % str(event))
+    etype = event['event_type']
+    attribute = event['attribute_name']
+    meta = event['meta']
+    if re.search( 'Retirement$', etype ) : # Unload the plugin
+        p = sg.find_one( meta['entity_type'], [[ 'id', 'is', meta['entity_id']]], ['sg_script_path'], retired_only=True )
         if p['sg_script_path'] and p['sg_script_path']['local_path'] :
             logger.info('Unloading %s', p['sg_script_path']['name'])
             args['engine'].unloadPlugin( p['sg_script_path']['local_path'])
-    elif e.search( 'Revival$', etype ) : #Should reload the plugin
-        p = sg.find_one( entity['type'], [[ 'id', 'is', entity['id']]], ['sg_script_path', 'sg_status_list'] )
+    elif re.search( 'Revival$', etype ) or re.search( 'New$', etype ): #Should reload the plugin
+        p = sg.find_one( meta['entity_type'], [[ 'id', 'is', meta['entity_id']]], ['sg_script_path', 'sg_status_list'] )
         if p['sg_script_path'] and p['sg_script_path']['local_path'] and p['sg_status_list'] == 'act' :
             logger.info('Loading %s', p['sg_script_path']['name'])
             args['engine'].loadPlugin( p['sg_script_path']['local_path'], autoDiscover=False)
-
-         
 
 
