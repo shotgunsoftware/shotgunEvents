b830c59bcbeb4a08bd7cba1fe4c9c99e73a08d93
Alexandra Lefève-Gourmelon <gou@mikrosimage.eu>
2015-03-16 12:51:01 +0100

    #3031: Clean package.py

--- package.py
+++ package.py
@@ -5,10 +5,5 @@ requires = ['shotgunPythonApi', 'shotgunEventPlugins']
 
 def commands():
 
-    import os
     env.PYTHONPATH.append(this.root)
-    env.SG_DAEMON_ROOT = this.root
-
     alias('sgDaemon', 'python {root}/src/shotgunEventDaemon.py')
-    # To be able to have a relative path in config
-    os.chdir(this.root)
