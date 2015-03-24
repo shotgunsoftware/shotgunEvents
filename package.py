856ceeb066baf8e7d5743aff409e4c6614b15fa4
Alexandra Lefève-Gourmelon <gou@mikrosimage.eu>
2015-03-24 12:07:38 +0100

    #3145: Fix package

--- package.py
+++ package.py
@@ -6,4 +6,4 @@ requires = ['shotgunPythonApi']
 def commands():
 
     env.PYTHONPATH.append(this.root)
-    alias('sgDaemon', 'python {root}/shotgunEvents/shotgunEventDaemon.py')
+    alias('sgDaemon', 'python {root}/shotgunEvents/src/shotgunEventDaemon.py')
